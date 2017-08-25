f90096281f911f253468914e8f49f0dbb11b7e35
mali44 <maalogy@gmail.com>
2017-08-25 01:57:18 -0700

    User can scan more than one website's meta informations Table aligns on left Scan shows Title Scan shows Description Metafunctions desperates in Appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,35 +20,81 @@ class ApplicationController < ActionController::Base
 
   end
 
-  def url_valid?(url)
-   url = URI.parse(url) rescue false
-   url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
 
- end
 
   def index_post
 
-
-    puts params[:comment]
-    links= params[:comment].split(/\n+/).each do |w|
+      page_links=nil
+      puts params[:comment]
+      all_links = Array.new(0)
+      title= Array.new(0)
+      description=Array.new(0)
+      links= params[:comment].split(/\n+/).each do |w|
       page_links = scrape_first(w.strip.to_s)
-      @page_links = page_links
-  end
-  render 'index'
+      all_links.push(page_links).each do |tl|
+      title.push(scrape_title(w.strip.to_s))
+      description.push(scrape_desc(w.strip.to_s))
+      end
+      puts all_links
 
 
-  end
 
-  def scrape_first(url)
+        end
+        @page_links=all_links
+        @title= title
+        @desc=description
 
-    begin
-      page = MetaInspector.new(url).to_s
-      match = page.scan(/href\s*=\s*"([^"]*)"/)
-      return match
+
+
+    render 'index'
+
+
+    end
+
+    def scrape_first(url)
+
+
+      page = MetaInspector.new(url)
+      all_links=page.links.all
+
+      return all_links
+
+
     rescue Exception => e
       false
-    end
+
   end
+
+  def scrape_title(url)
+
+
+      page = MetaInspector.new(url)
+      title= page.title
+
+      return title
+
+
+    rescue Exception => e
+      false
+
+  end
+
+
+    def scrape_desc(url)
+
+
+        page = MetaInspector.new(url)
+        desc= page.best_description
+
+        return desc
+
+
+      rescue Exception => e
+        false
+
+    end
+
+
 
 
     #  doc = Nokogiri::HTML(open("http://muhammetaliariturk.com/"))
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
+# encoding: utf-8
 # Load the Rails application.
 require_relative 'application'
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
